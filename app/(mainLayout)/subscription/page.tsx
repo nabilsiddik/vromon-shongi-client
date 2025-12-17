7b43a12cf3fe14a823073e25b2ceb2bf8891f6f1
nabilsiddik <nabilsiddik90@gmail.com>
2025-12-18 02:18:25 +0600

    vercel deploy

--- app/(mainLayout)/subscription/page.tsx
+++ app/(mainLayout)/subscription/page.tsx
@@ -1,20 +1,5 @@
 import SubscriptionButton from "@/components/shared/SubscriptionButton";
-import { serverFetch } from "@/lib/serverFetch";
-
-export async function handleSubscription(planType: "monthly" | "yearly") {
-  const res = await serverFetch.post(`/subscription/create-session`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ plan: planType }),
-  });
-
-  const data = await res.json();
-
-  console.log(data);
-
-  return data;
-}
+import { handleSubscription } from "@/services/subscription";
 
 const Subscription = () => {
   return (
---
+++ services/subscription/index.ts
@@ -0,0 +1,17 @@
+"use server";
+import { serverFetch } from "@/lib/serverFetch";
+
+export async function handleSubscription(planType: "monthly" | "yearly") {
+  const res = await serverFetch.post(`/subscription/create-session`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ plan: planType }),
+  });
+
+  const data = await res.json();
+
+  console.log(data);
+
+  return data;
+}
